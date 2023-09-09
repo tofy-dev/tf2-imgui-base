afe86f2125eb51410d52a19ed58a7506cebd05b8
tofy <tofy@disroot.org>
2023-09-09 13:59:15 -0700

    get it to work

--- src/runtime-x86/init.cpp
+++ src/runtime-x86/init.cpp
@@ -17,21 +17,20 @@ uintptr_t swapwindow_original;
 void hkSwapWindow(SDL_Window* window) {
 	// Get the original 'SDL_GL_SwapWindow' symbol from 'libSDL2-2.0.so.0'.
 	static void (*oSDL_GL_SwapWindow) (SDL_Window*) = reinterpret_cast<void(*)(SDL_Window*)>(swapwindow_original);
+    static std::ofstream ofs{"/tmp/imgui.txt"};
 	
 	// Store OpenGL contexts.
 	static SDL_GLContext original_context = SDL_GL_GetCurrentContext();
 	static SDL_GLContext user_context = NULL;
-    static std::ofstream ofs{"/tmp/imgui.txt"};
 	
 	// Perform first-time initialization.
 	if (!user_context) {
 		// Create a new context for our rendering.
-        ofs << "current context " << ImGui::GetCurrentContext() << std::endl;
 		user_context = SDL_GL_CreateContext(window);
         ImGui::CreateContext();
-        ofs << "current context " << ImGui::GetCurrentContext() << std::endl;
-        ofs << "user context " << user_context << std::endl;
+
         ImGui_ImplSDL2_InitForOpenGL(window, user_context);
+        ImGui_ImplOpenGL2_Init();
 	}
 
 	// Switch to our context.
@@ -42,8 +41,11 @@ void hkSwapWindow(SDL_Window* window) {
 	ImGui_ImplSDL2_NewFrame(window);
     ImGui::NewFrame();
 
-	ImGui::Text("Hello, world!");
+	ImGui::ShowDemoWindow();
+
 	ImGui::Render();
+    ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
 
 	// Swap back to the game context.
 	SDL_GL_MakeCurrent(window, original_context);
